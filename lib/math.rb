77fc6f20952194fddfc4b00ff0575f9e9745deff
Christian P Andrews <cpandrews91@gmail.com>
2020-07-28 09:38:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,65 @@
-def addition(num1, num2)
+class Cmath
+def addition(number1, number2)
+    sum = number1 + number2
+    return sum
+  end
 
+  def subtraction(number1, number2)
+    difference = number1 - number2
+    return subtraction
+  end
+
+  def multiplication(number1, number2)
+    product = number1 * number2
+    return product
+  end
+
+  def division(number1, number2)
+    quotient = number1 / number2
+    return quotient
+  end
+
+  def modulo(number1, number2)
+    remainder = number1 % number2
+    return remainder
+  end
+
+  def square_root(number1)
+    squareRoot = Cmath.sqrt(number1)
+    return squareRoot
+  end
 end
+#
+#
+#
+#
+#
 
-def subtraction(num1, num2)
+#class math
 
+#def addition(num1, num2)
+#sum = num1 + num2
+#return sum
+#end
+
+#def subtraction(num1, num2)
+#diff = num1 - num2
+#return diff
+#end
+
+def multiplication(num1, num2)
+product = num1 * num2
+return product
 end
 
 def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
+quotient = num1 / num2
+return quotient
 end
 
 def modulo(num1, num2)
-
+remainder = num1 % num2
+return remainder
 end
 
 def square_root(num)
